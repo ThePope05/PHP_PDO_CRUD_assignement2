3295e60eee1553710d228690b4a27647f1f52976
ThePope05 <swobben05@gmail.com>
2023-01-31 15:32:05 +0100

    Added basic styling

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -63,6 +67,8 @@
                 <input type="checkbox" name="spices" id="blackPeper" value="blackPeper">
                 <label for="blackPeper">Black peper</label>
             </div>
+
+            <button type="submit">Submit</button>
         </form>
     </fieldset>
 </body>
