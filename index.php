b15f17ed4b59b17e1de8951fda31fa587a6022a7
ThePope05 <swobben05@gmail.com>
2023-02-03 10:13:54 +0100

    Edit link
    You can now click the edit button and it will put the info from that row in to the form !cant update row yet!

--- index.php
+++ index.php
@@ -12,69 +12,9 @@
     <title>Document</title>
 </head>
 <body>
-    <fieldset>
-        <legend><h1>Make your own pizza</h1></legend>
-        <form action="phpFiles/create.php" method="post">
-
-            <!--SIZE DROPDOWN INPUTS-->
-            <h3>Choose your size</h3>
-            <select name="size">
-                <option value="">Make your choice</option>
-                <option value="20">20cm</option>
-                <option value="25">25cm</option>
-                <option value="30">30cm</option>
-                <option value="35">35cm</option>
-                <option value="40">40cm</option>
-            </select>
-
-            <!--SAUCE DROPDOWN INPUTS-->
-            <h3>Choose your sauce</h3>
-            <select name="sauce">
-                <option value="">Make your choice</option>
-                <option value="tomatoSauce">Tomato sauce</option>
-                <option value="extraTomatoSauce">Extra tomato sauce</option>
-                <option value="spicyTomatoSauce">Spicy tomato sauce</option>
-                <option value="bbqSauce">Bbq sauce</option>
-                <option value="cremeFraice">Creme fraiche</option>
-            </select>
-
-
-            <!--TOPPING RADIO INPUTS-->
-            <div>
-                <h3>Choose your topping</h3>
-                <input type="radio" name="topping" id="vegan" value="vegan">
-                <label for="vegan">Vegan</label>
-                
-                <input type="radio" name="topping" id="vegie" value="vegetables">
-                <label for="vegie">Vegetable</label>
-
-                <input type="radio" name="topping" id="meat" value="meat">
-                <label for="meat">Meat</label>
-            </div>
-
-
-            <!--SPICES CHECKBOX INPUTS-->
-            <div>
-                <h3>Choose your spices</h3>
-                <input type="checkbox" name="spice1" id="parsley" value="parsley">
-                <label for="parsley">Parsley</label>
-
-                <input type="checkbox" name="spice2" id="oregano" value="oregano">
-                <label for="oregano">Oregano</label>
-
-                <input type="checkbox" name="spice3" id="chiliFlakes" value="chiliFlakes">
-                <label for="chiliFlakes">Chilie flakes</label>
-
-                <input type="checkbox" name="spice4" id="blackPeper" value="blackPeper">
-                <label for="blackPeper">Black peper</label>
-            </div>
-
-            <button type="submit">Submit</button>
-        </form>
-    </fieldset>
-    <fieldset>
+    <fieldset class="scroll">
         <legend>
-            <h1>Others pizzas</h1>
+            <h1>All pizzas</h1>
         </legend>
         <table>
             <tr>
@@ -98,11 +38,14 @@
                         echo "<td>$pizza->sauce</td>";
                         echo "<td>$pizza->topping</td>";
                         echo "<td>$pizza->spices</td>";
-                        echo "<td><a><span class='material-symbols-outlined'>edit</span></a></td>";
+                        echo "<td><a href='createEditForm.php?id=$pizza->id'><span class='material-symbols-outlined'>edit</span></a></td>";
                         echo "<td><a href='phpFiles/delete.php?id=$pizza->id'><span class='material-symbols-outlined'>delete</span></a></td>";
                     echo "</tr>";
                 }
             ?>
+            <tr>
+                <td><a href="createEditForm.php"><span class='material-symbols-outlined'>add_circle</span></a></td>
+            </tr>
         </table>
     </fieldset>
 </body>
